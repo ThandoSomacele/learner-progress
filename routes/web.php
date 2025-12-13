525a8667fba07abfbc54777747b4dbd51eb1baef
Thando Somacele <thando.somacele@gmail.com>
2025-12-13 22:11:07 +0200

    Add route for learner progress dashboard
    Register GET route with named route for flexible URL management.
    Supports optional course_id and sort query parameters.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\LearnerProgressController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/learner-progress', [LearnerProgressController::class, 'index'])
+    ->name('learner-progress.index');
